3d96b2999f7d23ce09dcbd0cb5c2f305df77dd4b
Oukey <42565803+Oukey@users.noreply.github.com>
2018-11-07 22:28:27 +0400

    Update test_LinkList2.py
    revision

--- test_LinkList2.py
+++ test_LinkList2.py
@@ -18,8 +18,8 @@ class LinkedList2Test(unittest.TestCase):
         link_2.add_in_tail(Linked_List2.Node2(1))  # заполнение списка
         link_2.add_in_tail(Linked_List2.Node2(2))  # заполнение списка
         link_2.add_in_tail(Linked_List2.Node2(-3))  # заполнение списка
-        self.assertEqual(link_2.find_first(2), 2)  # проверка поиска по значению в пустом списке
-        self.assertEqual(link_2.find_first(-3), -3)  # проверка поиска отрицательного значения
+        self.assertEqual(link_2.find_first(2).value, 2)  # проверка поиска по значению в пустом списке
+        self.assertEqual(link_2.find_first(-3).value, -3)  # проверка поиска отрицательного значения
         self.assertIsNone(link_2.find_first(0))  # проверка поиска, если в списке нет искомого значения
 
     def test_del_first_0(self):  # тест удаления узла по значению №1
@@ -49,22 +49,31 @@ class LinkedList2Test(unittest.TestCase):
 
     def test_insert_next_0(self):  # тест вставки нового узла после заданного
         link_2 = Linked_List2.LinkedList2()  # создание пустого списка
-        link_2.insert_next(0, 111)  # добавление нового узла после заданного
-        self.assertIsNone(link_2.head)  # проверка добавления нового узла после заданного в пустом списке
+        n_n = Linked_List2.Node2(111)  # создание узла
+        link_2.insert_next(None, n_n)  # добавление нового узла после заданного
+        self.assertEqual(link_2.head.value, 111)  # проверка добавления нового узла после заданного в пустом списке
 
     def test_insert_next_1(self):  # тест вставки нового узла после заданного
         link_2 = Linked_List2.LinkedList2()  # создание пустого списка
-        link_2.add_in_tail(Linked_List2.Node2(1))  # заполнение списка
-        link_2.add_in_tail(Linked_List2.Node2(-2))  # заполнение списка
-        link_2.insert_next(0, 111)  # добавление нового узла после заданного
-        self.assertEqual(link_2.head.next.value, 111)  # проверка значения узла, после добавления нового
-        self.assertEqual(link_2.head.next.next.value, (-2))  # проверка значения узла, после добавления нового
+        n1 = Linked_List2.Node2(1)  # создание узла
+        n2 = Linked_List2.Node2(2)  # создание узла
+        n3 = Linked_List2.Node2(3)  # создание узла
+        n_n = Linked_List2.Node2(111)  # создание узла
+        link_2.add_in_tail(n1)  # заполнение списка
+        link_2.add_in_tail(n2)  # заполнение списка
+        link_2.add_in_tail(n3)  # заполнение списка
+        link_2.insert_next(n2, n_n)  # добавление нового узла после заданного
+        self.assertEqual(link_2.head.next.next.value, 111)  # проверка значения узла, после добавления нового
+        self.assertEqual(link_2.head.next.next.next.value, 3)  # проверка значения узла, после добавления нового
 
     def test_insert_next_2(self):  # тест вставки нового узла после последнего узла
         link_2 = Linked_List2.LinkedList2()  # создание пустого списка
-        link_2.add_in_tail(Linked_List2.Node2(-2))  # заполнение списка
-        link_2.add_in_tail(Linked_List2.Node2(3))  # заполнение списка
-        link_2.insert_next(1, 111)  # добавление нового узла после заданного
+        n1 = Linked_List2.Node2(1)  # создание узла
+        n2 = Linked_List2.Node2(2)  # создание узла
+        n_n = Linked_List2.Node2(111)  # создание узла
+        link_2.add_in_tail(n1)  # заполнение списка
+        link_2.add_in_tail(n2)  # заполнение списка
+        link_2.insert_next(n2, n_n)  # добавление нового узла после заданного
         self.assertEqual(link_2.head.next.next.value, 111)  # проверка значения узла, после добавления нового
         self.assertEqual(link_2.tail.value, 111)  # проверка значения хвостового узла
 
@@ -76,3 +85,6 @@ class LinkedList2Test(unittest.TestCase):
         link_2.insert_first(0)  # добавление узла в начало списка
         self.assertEqual(link_2.head.value, 0)  # проверка соответствия значения головного узла
         self.assertEqual(link_2.tail.value, 100)  # проверка соответствия значения хвостового узла
+
+    if __name__ == "__main__":
+        unittest.main()
