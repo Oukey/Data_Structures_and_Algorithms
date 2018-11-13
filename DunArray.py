6d966286b8f93ffb673ed29560088beecea70e1e
Oukey <42565803+Oukey@users.noreply.github.com>
2018-11-13 22:05:31 +0400

    Update DunArray.py

--- DunArray.py
+++ DunArray.py
@@ -31,23 +31,51 @@ class DynArray:
         self.array[self.count] = itm
         self.count += 1
 
-    def insert(self, ind, itm):
-        pass
+    '''
+    4.1. Добавьте елемент insert(i, itm), который вставляет в i-ю позицию объект itm, сдвигая вперёд все последующие 
+    элементы. Учтите, что новая длина массива может привысить размер буфера.
+    '''
+
+    def insert(self, i, itm):
+        new_array = []
+        for el in range(self.count):
+            if self.count == self.capacity:
+                self.resize(2 * self.capacity)
+            elif el == i:
+                new_array.append(itm)
+                self.count += 1
+            new_array.append(el)
+        self.array = new_array
+        return self.array
+
+    '''
+    4.2. Добавьте метод delete(i), который удаляет объект из i-й позициию Если количество элементов массива стало в 
+    два или более раз меньше его потенциальной ёмкости, выполните сжатие буфера, сохраняя минимальную ёмкость 
+    16 элементов.
+    '''
 
     def delete(self, ind):
-        pass
+        new_array = []
+        for el in range(self.count):
+            if el == ind:
+                self.count -= 1
+                continue
+            elif self.capacity / self.count >= 2 and self.capacity / 2 > 16:
+                self.capacity /= 2
+            new_array.append(self.array[el])
+        self.array = new_array
+        return self.array
 
 
 da = DynArray()
-for i in range(6):
+for i in range(2):
     da.append(i)
     print(da[i])
 
-'''
-Задание:
-4.1. Добавьте елемент insert(i, itm), который вставляет в i-ю позицию объект itm, сдвигая вперёд все последующие 
-элементы. Учтите, что новая длина массива может привысить размер буфера.
+da.insert(1, 111)
+for i in da:
+    print(i)
 
-4.2. Добавьте метод delete(i), который удаляет объект из i-й позициию Если количество элементов массива стало в два или
-более раз меньше его потенциальной ёмкости, выполните сжатие буфера, сохраняя минимальную ёмкость 16 элементов.
-'''
+da.delete(0)
+for i in da:
+    print(i)
