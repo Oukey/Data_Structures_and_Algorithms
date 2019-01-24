ac8431c2a0bde320c8548ca4f0f1a6e9d1a223d9
Oukey <42565803+Oukey@users.noreply.github.com>
2019-01-24 23:09:38 +0400

    Update New_DLL.py

--- New_DLL.py
+++ New_DLL.py
@@ -20,14 +20,14 @@ class LinkedList2:
             item.prev = self.tail
         self.tail = item
 
-    def find(self, val):  # метод поиска певого узла по его значению
+    def find(self, val):  # метод поиска первого узла по его значению
         node = self.head
         while node is not None:
-            if node.valut == val:
+            if node.value == val:
                 return node
             node = node.next
 
-    def find_all(self, val):  # метод поиска всех узлов по значению (возвращает список найдённых узлов)
+    def find_all(self, val):  # метод поиска всех узлов по значению (возвращает список найденных узлов)
         node = self.head
         list_node = []
         while node is not None:
@@ -39,89 +39,76 @@ class LinkedList2:
     def delete(self, val, all=False):  # метод удаления узла по значению
         if self.head is not None:  # если список не пустой
             node = self.head
-            if node.value == val and node.next is None:  # если список из 1 кзла и значение равняется искомому
+            if node.value == val and node.next is None:  # если список из 1 узла и значение равняется искомому
                 self.clean()  # очистить список
                 return
             while node is not None:
-                # 1. Если найден первыый узел
-                if node.value == val and node == self.head:
-
-                    pass
-                # 2. Если найден средний узел
-                elif node.value == val and node == self.tail:
-                    pass
+                # 1. Если найден средний узел
+                if node.value == val and node != self.head and node != self.tail:
+                    node.prev.next = node.next
+                    node.next.prev = node.prev
+                    if all is False:
+                        return
+                    else:
+                        node = node.next
+                        continue
+                # 2. Если найден первый узел
+                elif node.value == val and node == self.head:
+                    node.next.prev = None
+                    self.head = node.next
+                    if all is False:
+                        return
+                    else:
+                        node = node.next
+                        continue
                 # 3. Если найден хвостовой узел
-                elif node.value == val and self.head != node != self.tail:
-                    pass
+                elif node.value == val and node == self.tail:
+                    node.prev.next = None
+                    self.tail = node.prev
+                    if all is False:
+                        return
+                    else:
+                        node = node.next
+                        continue
 
-        # if all == False:
-        #     node = self.find(val)
-        # else:
-        #     node = self.find_all()
-        #     while node.next is not None:
-        #         if node is not None:
-        #             if node == self.head:  # еси найден первый узел
-        #                 self.head = node.next
-        #                 node.next.prev = None
-        #             elif node != self.head and node != self.tail:  # Если найден средний узел
-        #                 node.prev.next = node.next
-        #                 node.next.prev = node.prev
-        #             elif node == self.tail:  # если найден последний узел
-        #                 self.tail = node.prev
-        #                 node.prev.next = None
-        #
-        # # while node is not None:
-        # #     if node.value == val:
-        #
-        # pass  # здесь будет ваш код
-
-        def clean(self):
-            self.__init__()
-
-        def len(self):
-            node = self.head
-            ln = 0
-            while node is not None:
-                ln += 1
                 node = node.next
-            return ln
 
-        def insert(self, afterNode, newNode):
-            if self.head is None and afterNode is None:
-                self.head = newNode
-                self.tail = newNode
-            elif self.head is not None and afterNode is None:
-                newNode.prev = self.tail
-                self.tail.next = newNode
-                self.tail = newNode
-            elif self.head is not None:
-                if afterNode == self.tail:
-                    self.add_in_tail(newNode)
-                    return
-                else:
-                    newNode.next = afterNode.next
-                    afterNode.next.prev = newNode
-                    afterNode.next = newNode
-                    newNode.prev = afterNode
-                    return
-                    # if afterNode == self.head:
-                #     afterNode.next = newNode
-                #     newNode.prev = afterNode
-                #     self.tail = newNode
-                # else:
-                #     newNode.next = afterNode.next
-                #     afterNode.next.prev = newNode
-                #     afterNode.next = newNode
+    def clean(self):
+        self.__init__()
 
-        def add_in_head(self, newNode):
-            newNode.next = self.head
-            self.head.prev = newNode
+    def len(self):
+        node = self.head
+        ln = 0
+        while node is not None:
+            ln += 1
+            node = node.next
+        return ln
+
+    def insert(self, afterNode, newNode):
+        if self.head is None and afterNode is None:
             self.head = newNode
+            self.tail = newNode
+        elif self.head is not None and afterNode is None:
+            self.add_in_tail(newNode)
+        elif self.head is not None:
+            if afterNode == self.tail:
+                self.add_in_tail(newNode)
+                return
+            else:
+                newNode.next = afterNode.next
+                afterNode.next.prev = newNode
+                afterNode.next = newNode
+                newNode.prev = afterNode
 
-    '''
-     2.5. Добавьте в класс LinkedList2 метод вставки узла после заданного узла.
-    insert(afterNode, newNode)
-    Если afterNode = None и список пустой, добавьте новый элемент первым в списке.
-    Если afterNode = None и список непустой, добавьте новый элемент последним в списке.
-    '''
+    def add_in_head(self, newNode):
+        newNode.next = self.head
+        self.head.prev = newNode
+        self.head = newNode
 
+    def print_first(self):
+        node = self.head
+        list_print = []
+        while node is not None:
+            list_print.append(node.value)
+            node = node.next
+        return list_print
