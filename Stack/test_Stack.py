614386a61fd2f8aa617cafd14c3f95def04cb754
Oukey <42565803+Oukey@users.noreply.github.com>
2019-02-07 22:25:44 +0400

    Update test_Stack.py

--- Stack/test_Stack.py
+++ Stack/test_Stack.py
@@ -4,6 +4,23 @@ import Stack
 
 
 class StackTest(unittest.TestCase):
+    
+    def test_is_empty(self):
+        s_1 = Stack.Stack()
+        self.assertTrue(s_1.is_empty())
+        s_1.push(1)
+        self.assertFalse(s_1.is_empty())
+
+    def test_size(self):
+        s_2 = Stack.Stack()
+        self.assertEqual(s_2.size(), 0)
+        s_2.push(1)
+        s_2.push(2)
+        self.assertEqual(s_2.size(), 2)
+        s_2.pop()
+        self.assertEqual(s_2.size(), 1)
+        s_2.peek()
+        self.assertEqual(s_2.size(), 1)
 
     def test_push(self):
         pass
