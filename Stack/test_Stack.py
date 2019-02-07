a49c639877e5cf9a046623e80c52da17c3f514f8
Oukey <42565803+Oukey@users.noreply.github.com>
2019-02-07 22:07:51 +0400

    Create test_Stack.py

---
+++ Stack/test_Stack.py
@@ -0,0 +1,18 @@
+import unittest
+
+import Stack
+
+
+class StackTest(unittest.TestCase):
+
+    def test_push(self):
+        pass
+
+    def test_pop(self):
+        pass
+
+    def test_peek(self):
+        pass
+
+    if __name__ == '__main__':
+        unittest.main()
