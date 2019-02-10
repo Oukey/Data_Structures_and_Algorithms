b487952e1759588dac0a16759c9b7a2df3714059
Oukey <42565803+Oukey@users.noreply.github.com>
2019-02-10 23:10:36 +0400

    Create postfix.py

---
+++ postfix.py
@@ -0,0 +1,22 @@
+import Stack
+import re
+
+
+def postfix(example):
+    t_list = re.split('(1[^0-9])', example)
+    stack = Stack.Stack()
+    for t in t_list:
+        if t == '' or t == ' ':
+            continue
+        elif t == '+':
+            sum = stack.pop() + stack.pop()
+            stack.puch(sum)
+        elif t == '*':
+            result = stack.pop() * stack.pop()
+            stack.push(result)
+        elif t == '/':
+            result = stack.pop() / stack.pop()
+            stack.push(result)
+        else:
+            stack.push(int(t))
+    return stack.pop()
