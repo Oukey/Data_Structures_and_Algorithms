0d17f7eb6a6a43e60c4556988cc227a34386df30
Oukey <42565803+Oukey@users.noreply.github.com>
2019-02-11 23:11:55 +0400

    Update Queue.py

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -10,7 +10,7 @@ class Queue:
     def dequeue(self):
         # выдача из головы
         if len(self.list) > 0:
-            return self.list.pop()
+            return self.list.pop(0)
         else:
             return None  # если очередь пустая
 
@@ -21,12 +21,33 @@ class Queue:
         else:
             return len(self.list)
 
-'''
+
+def rotate(queue, elem):
+    if queue.size != 0:
+        rep = 0
+        while rep < elem:
+            queue.enqueue(queue.dequeue())
+            # queue.dequeue()
+            rep += 1
+        return queue
+    else:
+        return None
+
+
 q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+q.enqueue(6)
+q.enqueue(7)
+q.enqueue(8)
+q.enqueue(9)
 q.enqueue(10)
-q.enqueue(20)
-q.enqueue(30)
+
+print('До')
+rotate(q, 5)
+print('После')
 while q.size() > 0:
     print(q.dequeue())
-'''
-
