0cdd02bb749f95d7facb84a16df13ea753247468
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-02 23:06:10 +0400

    Create Hash_Table_.py

---
+++ hashing/Hash_Table_.py
@@ -0,0 +1,28 @@
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        return self.slots.index(None)
+
+    def seek_slot(self, value):
+        if self.slots.count(None) != 0:
+            return self.slots.index(None)
+        else:
+            return None
+
+    def put(self, value):
+        i = self.seek_slot(value)
+        if i != None:
+            self.slots[i] = value
+            return i
+        else:
+            return None
+
+    def find(self, value):
+        if value in self.slots:
+            return self.slots.index(value)
+        else:
+            return None
