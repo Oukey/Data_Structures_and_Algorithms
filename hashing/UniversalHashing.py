572cf5acc31f74daa0e2c23a7992d0bf562f1669
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-06 21:35:55 +0400

    Update UniversalHashing.py

--- hashing/UniversalHashing.py
+++ hashing/UniversalHashing.py
@@ -1,1 +1,21 @@
+# Проект модели хзш-таблицы с применением
+# универсального хэширования
 
+
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        pass
+
+    def see_slot(self, value):
+        pass
+
+    def put(self, value):
+        pass
+
+    def find(self, value):
+        pass
