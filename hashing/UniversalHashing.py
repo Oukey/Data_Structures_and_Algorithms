47ae678cda330bb467194abfa1d6682904c214d4
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-09 00:12:52 +0400

    Update UniversalHashing.py

--- hashing/UniversalHashing.py
+++ hashing/UniversalHashing.py
@@ -1,15 +1,25 @@
 # Проект модели хзш-таблицы с применением
 # универсального хэширования
 
+import random
+
 
 class HashTable:
     def __init__(self, sz, stp):
         self.size = sz
         self.step = stp
         self.slots = [None] * self.size
+        self.options_int = lambda value: ((value * random.randrange(1, 17, 2) + random.randint(0,
+                                                                                               17)) % 17) % self.size
+        self.options_str = lambda value: hash(value) % self.size
 
     def hash_fun(self, value):
-        pass
+        fun = None
+        if type(value) == int:
+            fun = self.options_int(value)
+        elif type(value) == str:
+            fun = self.options_str(value)
+        return fun
 
     def see_slot(self, value):
         pass
@@ -19,3 +29,4 @@ class HashTable:
 
     def find(self, value):
         pass
+    
