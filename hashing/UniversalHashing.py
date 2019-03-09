5ad48c7a38ef5dcce5d18b575946e07b83424958
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-10 02:18:36 +0400

    Update UniversalHashing.py

--- hashing/UniversalHashing.py
+++ hashing/UniversalHashing.py
@@ -9,20 +9,32 @@ class HashTable:
         self.size = sz
         self.step = stp
         self.slots = [None] * self.size
-        self.options_int = lambda value: ((value * random.randrange(1, 17, 2) + random.randint(0,
-                                                                                               17)) % 17) % self.size
-        self.options_str = lambda value: hash(value) % self.size
+        self.num = random.choice((2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53))
+        self.options_int = lambda value: ((value * random.randrange(1, self.num, 2) + random.randint(0,
+                                                                                                     self.num)) % self.num) % self.size
+        self.options_str = {0: lambda value: hash(value) % self.size,
+                            1: lambda value: ((len(value) * self.num + self.step) % self.num) % self.size,
+                            2: lambda value: (len(value) * self.num + self.step - 1) % self.size
+                            }
 
     def hash_fun(self, value):
         fun = None
         if type(value) == int:
             fun = self.options_int(value)
         elif type(value) == str:
-            fun = self.options_str(value)
+            fun = random.choice(self.options_str)(value)
         return fun
 
     def seek_slot(self, value):
-        pass
+        slot = self.hash_fun(value)
+        buffer = 0
+        while buffer < self.size:
+            if self.slots[slot] is None:
+                return slot
+            slot += self.step
+            if slot > self.size - 1:
+                slot -= self.size
+            buffer += 1
 
     def put(self, value):
         ind = self.seek_slot(value)
@@ -37,4 +49,3 @@ class HashTable:
             return self.slots.index(value)
         else:
             return None
-    
