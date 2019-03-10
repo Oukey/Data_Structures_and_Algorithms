5a9887a1192d2e26faba77f45789787f1871ce49
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-10 19:37:25 +0400

    Update UniversalHashing.py

--- hashing/UniversalHashing.py
+++ hashing/UniversalHashing.py
@@ -1,6 +1,3 @@
-# Проект модели хзш-таблицы с применением
-# универсального хэширования
-
 import random
 
 
@@ -9,23 +6,27 @@ class HashTable:
         self.size = sz
         self.step = stp
         self.slots = [None] * self.size
-        self.num = random.choice((2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53))
-        self.options_int = lambda value: ((value * random.randrange(1, self.num, 2) + random.randint(0,
-                                                                                                     self.num)) % self.num) % self.size
-        self.options_str = {0: lambda value: hash(value) % self.size,
-                            1: lambda value: ((len(value) * self.num + self.step) % self.num) % self.size,
-                            2: lambda value: (len(value) * self.num + self.step - 1) % self.size
-                            }
+        self.options_int = random.choice([  # выбор из списока хэш_функций для цифр и пустой строки
+            lambda value: int((value * 37 + 29 % 11) % (self.size + 1)),
+            lambda value: int(((11 ** value * self.step + self.step) % self.size)),
+            lambda value: int((((123 + value + self.step) * self.step) % self.size)),
+        ])
+        self.options_str = random.choice([  # выбор из списока хэш_функций для строк
+            lambda value: (41 + len(value) + ord(value[0]) + ord(value[len(value) - 1]) ** 11) % self.size,
+            lambda value: ((len(value) ** 7 + ord(value[0]) ** 7 + ord(value[len(value) - 1]) ** 7) % 11) % self.size,
+            lambda value: (17 + len(value) + ord(value[0]) + ord(value[len(value) - 1]) ** 123) % self.size,
+        ])
 
-    def hash_fun(self, value):
-        fun = None
-        if type(value) == int:
-            fun = self.options_int(value)
-        elif type(value) == str:
-            fun = random.choice(self.options_str)(value)
-        return fun
+    def hash_fun(self, value):  # функция возвращает хэш функцию по типу вводимых данных
+        if type(value) == int or type(value) == float:  # для чисел
+            return self.options_int(value)
+        else:
+            if value == '':  # для пустой строки
+                return self.options_int(0)
+            else:  # для строк
+                return self.options_str(value)
 
-    def seek_slot(self, value):
+    def seek_slot(self, value):  # находит индекс пустого слота для значения, или None
         slot = self.hash_fun(value)
         buffer = 0
         while buffer < self.size:
@@ -35,17 +36,22 @@ class HashTable:
             if slot > self.size - 1:
                 slot -= self.size
             buffer += 1
+        return None
 
     def put(self, value):
-        ind = self.seek_slot(value)
-        if ind is not None:
-            self.slots[ind] = value
-            return ind
-        else:
+        slot = self.seek_slot(value)
+        if slot is not None:
+            self.slots[slot] = value  # записываем значение по хэш-функции
+            return slot  # возвращается индекс слота или None,
+        else:  # если из-за коллизий элемент не удаётся разместить
             return None
 
     def find(self, value):
-        if value in self.slots:
-            return self.slots.index(value)
-        else:
-            return None
+        # находит индекс слота со значением, или None
+        slot = self.hash_fun(value)
+        if self.slots[slot] == value:
+            return slot
+        for slt in range(self.size):
+            if self.slots[slt] == value:
+                return slt
+        return None
