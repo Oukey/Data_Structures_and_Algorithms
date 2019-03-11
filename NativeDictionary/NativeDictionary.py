3bbb7e24d3f8fefa3350e317a036d006d9cb39af
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-11 22:44:29 +0400

    Update NativeDictionary.py

--- NativeDictionary/NativeDictionary.py
+++ NativeDictionary/NativeDictionary.py
@@ -10,15 +10,20 @@ class NativeDictionary:
     def hash_fun(self, key):  # резерв
         return self.slots_key.index(None)
 
-    def seek_slot(self, key):  # функция определения свободного слота (попорядку)
-        if self.slots_key.count(None) != 0:
-            return self.slots_key.index(None)
+    def seek_slot(self, key):  # функция определения свободного слота
+        if self.is_key(key) is False:
+            if self.slots_key.count(None) != 0:
+                return self.slots_key.index(None)
+            else:
+                return None  # если массив заполнен
         else:
-            return None
+            return True
 
     def put(self, key, value):  # сохранение внутри класса ассоциативного массива пары ключ-значение
         slot = self.seek_slot(key)
-        if slot is not None:
+        if slot is True:
+            self.slots_value[self.slots_key.index(key)] = value
+        elif slot is not None:
             self.slots_key[slot] = key
             self.slots_value[slot] = value
             return slot
@@ -27,12 +32,12 @@ class NativeDictionary:
 
     def is_key(self, key):  # проверка, имеется ли в слотах такой ключ
         if key in self.slots_key:
-            return self.slots_key.index(key)
+            return True
         else:
-            return None
+            return False
 
     def get(self, key):  # поиск и извлечение значения по ключу или отсутствие значения если ключ не найден
-        if self.is_key(key) is not None:
-            return self.slots_value(self.is_key(key))
+        if self.is_key(key) is True:
+            return self.slots_value[self.slots_key.index(key)]
         else:
             return None
