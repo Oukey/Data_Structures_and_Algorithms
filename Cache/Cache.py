9b4cb6105a3250c6a2145ff7ea853d5961f8e2b9
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-22 21:27:12 +0400

    Create Cache.py

---
+++ Cache/Cache.py
@@ -0,0 +1,6 @@
+class NativeCache:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+        self.hits = [0] * self.size
