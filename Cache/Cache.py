56aafe912975312e25d10e7b09d9c20fc21ae3df
Oukey <42565803+Oukey@users.noreply.github.com>
2019-03-24 09:52:28 +0400

    Update Cache.py

--- Cache/Cache.py
+++ Cache/Cache.py
@@ -8,32 +8,47 @@ class NativeCache:
         self.values = [None] * self.size
         self.hits = [0] * self.size
 
-    def hash_fun(self, key):
-        if self.is_key(key) is False:
-            if self.slots.count(None) != 0:
-                return self.slots.index(None)
-            else:
-                return self.hits.index(min(self.hits))
-        else:
-            return None
-
     def put(self, key, value):
-        slot = self.hash_fun(value)
-        if slot is not None:
-            self.slots[slot] = key
-            self.values[slot] = value
-            self.hits[slot] = 1
-            return slot
+        if self.is_key(key) is True:
+            slot = self.find(key)
+        else:
+            slot = self.seek_fun(key)
+        self.slots[slot] = key
+        self.values[slot] = value
+        self.hits[slot] = 1
+
+    def hash_fun(self, key):
+        code = 0
+        for c in str(key):
+            code = code * 11 + ord(c)
+        return code % self.size
+
+    def seek_fun(self, key):
+        if self.slots.count(None) == 0:
+            return self.hits.index(min(self.hits))
+        slot = self.hash_fun(key)
+        count = 0
+        while count < self.size:
+            if self.slots[slot] is None:
+                return slot
+            slot += 1
+            if slot + 1 > self.size:
+                slot -= self.size
+            count += 0
+
+    def find(self, key):
+        for i in range(self.size):
+            if self.slots[i] == key:
+                return i
 
     def is_key(self, key):
-        if key in self.slots:
+        if self.find(key) is not None:
             return True
         else:
             return False
 
     def get(self, key):
-        if self.is_key(key) is True:
-            self.hits[self.slots.index(key)] += 1
-            return self.values[self.slots.index(key)]
-        else:
-            return None
+        if self.is_key(key) is not False:
+            slot = self.find(key)
+            self.hits[slot] += 1
+            return self.values[slot]
