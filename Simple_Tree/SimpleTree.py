1714be17ecd6ef5b6522fb711abd9c3518d8db28
Oukey <42565803+Oukey@users.noreply.github.com>
2019-04-01 22:56:00 +0400

    Update SimpleTree.py

--- Simple_Tree/SimpleTree.py
+++ Simple_Tree/SimpleTree.py
@@ -16,11 +16,18 @@ class SimpleTree:
 
     def AddChild(self, ParentNode, NewChild):
         # метод добавления нового дочернего узла существующему ParentNode
-        pass
+        if NewChild is not ParentNode:
+            ParentNode.Children.append(NewChild)  # добавление в список дочерних узлов
+            NewChild.Parent = ParentNode  # ссылка на родительскую ветвь для нового узла
 
     def DeleteNode(self, NodeToDelete):
         # метод удаления существующего узла NodeToDelete
-        pass
+        # ВОПРОС!!! Удалять ли дочерние узлы уза???
+        if NodeToDelete.Parent is not None:
+            parent = NodeToDelete  # определение переменной, хранящей ссылку на родительский узел
+            parent.Children.remove(NodeToDelete)  # для родительского узла удалить заданный дочерний узел
+            NodeToDelete.Parent = None  # удалить ссылку на родительский узел
+            NodeToDelete.Children = []  # обнулить список дочерних узлов
 
     def GetAllNodes(self):
         # метод выдачи всех узлов дерева в определенном порядке
