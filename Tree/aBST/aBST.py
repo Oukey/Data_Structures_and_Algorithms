c36c710cdf757aef2fdc26518850aeb39c1749e3
Eugene <42565803+Oukey@users.noreply.github.com>
2019-04-24 21:41:21 +0400

    Update aBST.py

--- Tree/aBST/aBST.py
+++ Tree/aBST/aBST.py
@@ -1,37 +1,37 @@
 # метод двоичного дерева поиска
 
-
 class aBST:
 
     def __init__(self, depth):
-        tree_size = 2 ** (depth + 1) - 1  # рассчет количество узлов по заданной глубине
+        # правильно рассчитайте размер массива для дерева глубины depth:
+        tree_size = 2 ** (depth + 1) - 1
         self.Tree = [None] * tree_size  # массив ключей
 
     def FindKeyIndex(self, key):
         '''
         Метод поиска ключа в массиве
         Параметр "key" - искомый ключ
-        return - индекс с найденным ключем или None если ключ не найден
+        return - индекс с найденным ключем, индекс *(-1) если найден свободный индекс или None если ключ не найден
         '''
-        if self.Tree[0] is None:
+        if self.Tree[0] is None:  # если дерево не заполнено
             return 0
         elem = 0
         while elem < len(self.Tree):
+            # если найдено свободное мето
+            if self.Tree[elem] is None:
+                return - elem
             # если ключ найден
             if key == self.Tree[elem]:
                 return elem
-            # если найден свободный узел
-            elif self.Tree[elem] is None:
-                return - elem
-            # если заданный ключ меньше текущего - переход к левому потомку
-            elif key < self.Tree[elem]:
-                elem = 2 * elem + 1
-                continue
-            # если заданный ключ больше текущего - переход к правому потомку
-            elif key > self.Tree[elem]:
+            # если искомый ключ больше текущего
+            if key > self.Tree[elem]:
                 elem = 2 * elem + 2
                 continue
-        return None
+            # если искомый ключ меньше текущего
+            if key < self.Tree[elem]:
+                elem = 2 * elem + 1
+                continue
+        return None  # если свободных узлов и совпадений нет
 
     def AddKey(self, key):
         '''
@@ -40,8 +40,12 @@ class aBST:
         return - индекс добавленного/существующего ключа или -1 если добавление не удалось
         '''
         result = self.FindKeyIndex(key)
-        if result is None or result > 0:
+        # если не найдено свободных узлов
+        if result is None:
             return -1
-        elif result <= 0:
-            self.Tree[abs(result)] = key
+        else:
+            # если найден узел для добавления нового ключа
+            if self.Tree[abs(result)] is None:
+                self.Tree[abs(result)] = key
             return abs(result)
+
