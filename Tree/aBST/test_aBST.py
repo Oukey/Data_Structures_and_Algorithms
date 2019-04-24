175543d18818b820e5058316d82363c6e707b4a1
Eugene <42565803+Oukey@users.noreply.github.com>
2019-04-24 21:42:02 +0400

    Update test_aBST.py

--- Tree/aBST/test_aBST.py
+++ Tree/aBST/test_aBST.py
@@ -19,12 +19,24 @@ class aBSTTest(unittest.TestCase):
     def test_Find(self):
         tr = aBST.aBST(2)
         self.assertEqual(tr.FindKeyIndex(50), 0)  # возвращает индекс 0 при пустом дереве
-        self.assertEqual(tr.AddKey(50), 1)
+        self.assertEqual(tr.AddKey(50), 0)
         tr.AddKey(50)
-        self.assertEqual(tr.AddKey(50), -1)
+        self.assertEqual(tr.AddKey(50), 0)
         self.assertEqual(tr.FindKeyIndex(50), 0)  # возвращает -1 если
         self.assertEqual((tr.FindKeyIndex(25)), -1)
         self.assertEqual((tr.FindKeyIndex(75)), -2)
+        tr.AddKey(25)
+        tr.AddKey(75)
+        self.assertEqual((tr.FindKeyIndex(25)), 1)
+        self.assertEqual((tr.FindKeyIndex(75)), 2)
+        tr.AddKey(30)
+        tr.AddKey(5)
+        tr.AddKey(51)
+        tr.AddKey(78)
+        self.assertIsNone(tr.FindKeyIndex(1))
+        self.assertIsNone(tr.FindKeyIndex(100))
+        self.assertEqual(tr.AddKey(1), -1)
+        self.assertEqual(tr.AddKey(100), -1)
 
     if __name__ == '__main__':
         unittest.main()
