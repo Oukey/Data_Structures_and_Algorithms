2411d59558c7959eb186d538345c8b2f03d06acc
Eugene <42565803+Oukey@users.noreply.github.com>
2019-04-27 12:35:52 +0400

    Update aBST.py

--- Tree/aBST/aBST.py
+++ Tree/aBST/aBST.py
@@ -1,59 +1,37 @@
-# метод двоичного дерева поиска
+# модель преобразования массива под структуру BST
 
-class aBST:
 
-    def __init__(self, depth):
-        # правильно рассчитайте размер массива для дерева глубины depth:
-        tree_size = 2 ** (depth + 1) - 1
-        self.Tree = [None] * tree_size  # массив ключей
+def depth_calc(a):
+    '''
+    модуль расчета глубины
+    параметр a - исходный массив
+    возвращает кортеж с параметрами: 0 - глубина дерева, 1 - длина массива
+    '''
+    if a:
+        len_array = 0
+        depth_tree = 0
+        while len_array < len(a):
+            depth_tree += 1
+            len_array = 2 ** (depth_tree + 1) - 1
+        return depth_tree, len_array
 
-    def FindKeyIndex(self, key):
-        '''
-        Метод поиска ключа в массиве
-        Параметр "key" - искомый ключ
-        return - индекс с найденным ключем, индекс *(-1) если найден свободный индекс или None если ключ не найден
-        '''
-        if self.Tree[0] is None:  # если дерево не заполнено
-            return 0
-        elem = 0
-        while elem < len(self.Tree):
-            # если найдено свободное мето
-            if self.Tree[elem] is None:
-                return - elem
-            # если ключ найден
-            if key == self.Tree[elem]:
-                return elem
-            # если искомый ключ больше текущего
-            if key > self.Tree[elem]:
-                elem = 2 * elem + 2
-                continue
-            # если искомый ключ меньше текущего
-            if key < self.Tree[elem]:
-                elem = 2 * elem + 1
-                continue
-        return None  # если свободных узлов и совпадений нет
 
-    def AddKey(self, key):
-        '''
-        Метод добавления ключа в массив
-        Параметр "key" - заданный ключ
-        return - индекс добавленного/существующего ключа или -1 если добавление не удалось
-        '''
-        result = self.FindKeyIndex(key)
-        # если не найдено свободных узлов
-        if result is None:
-            return -1
-        else:
-            # если найден узел для добавления нового ключа
-            if self.Tree[abs(result)] is None:
-                self.Tree[abs(result)] = key
-            return abs(result)
+def binary_sort(array_BST, a, ind):
+    if a:
+        array_BST[ind] = a[len(a) // 2]
+        binary_sort(array_BST, a[:len(a) // 2], 2 * ind + 1)
+        binary_sort(array_BST, a[len(a) // 2 + 1:], 2 * ind + 2)
+    else:
+        return None
 
-        
+
 def GenerateBBSTArray(a):
     '''
-    Функция сортировки массаива под структуру BST
+    Функция сортировки массива под структуру BST
     Параметр (a) неотсортированный массив
-    return - массив, содержащий структуру сбалансированного BST
+    return - массив со структурой сбалансированного BST
     '''
-    return None
+    a.sort()
+    array_BST = [None] * depth_calc(a)[1]
+    binary_sort(array_BST, a, 0)
+    return array_BST
