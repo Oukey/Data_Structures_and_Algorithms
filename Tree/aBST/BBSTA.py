e1cb42f2c888f667149f5ea2bdba47b79a17349e
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-11 21:14:37 +0400

    Update BBSTA.py

--- Tree/aBST/BBSTA.py
+++ Tree/aBST/BBSTA.py
@@ -10,6 +10,10 @@ class BSTNode:
         self.RightChild = None  # правый потомок
         self.Level = 0  # уровень узла
 
+    def __repr__(self):
+        '''метод формального обозначения объекта'''
+        return '_{}'.format(self.NodeKey)
+
 
 class BalancedBST:
 
@@ -17,10 +21,10 @@ class BalancedBST:
         self.Root = None
         self.BSTArray = []  # временный массив для ключей дерева
 
-    def depth_calc(self, array):
+    def depth_calc(self, array):  # доп. метод
         '''
         Метод расчета глубины
-        парамет array - исходный массив
+        параметр array - исходный массив
         возвращает кортеж с параметрами: 0 - глубина дерева, 1 - длина массива
         '''
         if array:
@@ -33,7 +37,7 @@ class BalancedBST:
         else:
             return 0, 0
 
-    def binary_sort(self, array_BST, array, ind):
+    def binary_sort(self, array_BST, array, ind):  # доп. метод
         '''Метод сортировки массива под структуру BST'''
         if array:
             array_BST[ind] = array[len(array) // 2]
@@ -52,8 +56,8 @@ class BalancedBST:
     def GenerateTree(self):
         '''Метод создания дерева с нуля из массива BSTArray'''
         self.Root = self.tree_builder(0)
-    
-        def tree_builder(self, ind, parent=None):
+
+    def tree_builder(self, ind, parent=None):
         '''Метод рекурсивного построения дерева из массива с '''
         if len(self.BSTArray) > ind:
             node = BSTNode(self.BSTArray[ind], parent)
@@ -70,4 +74,31 @@ class BalancedBST:
         Метод проверки баланса дерева с корнем root_node
         return True если сбалансировано, иначе возвращает False
         '''
-        return False  # сбалансировано ли дерево с корнем root_node
+        max_level = max(self.level_check(root_node)[1])
+        min_level = min(self.level_check(root_node)[1])
+        if abs(max_level - min_level) <= 1:
+            return True
+        else:
+            return False
+
+    def in_order(self, root_node):
+        '''Метод симметричного обхода в ширину: левый, корень, правый'''
+        if root_node.LeftChild:
+            yield from self.in_order(root_node.LeftChild)
+        yield root_node
+        if root_node.RightChild:
+            yield from self.in_order(root_node.RightChild)
+
+    def level_check(self, root_node):
+        '''
+        Метод определения листьев и несбалансированных узлов в дереве с корнем root_node
+        return: [[список листьев и несбалансированных узлов],[список уровней найденных узлов]]
+        '''
+        leaf_list = []
+        level_list = []
+        for next_node in self.in_order(root_node):
+            if next_node.NodeKey is not None:
+                if next_node.LeftChild is None or next_node.RightChild is None:
+                    leaf_list.append(next_node)  # список листьев и несбалансированных узлов (для проверки)
+                    level_list.append(next_node.Level)  # список уровней найденных узлов
+        return [leaf_list, level_list]
