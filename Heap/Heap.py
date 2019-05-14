9cb432532edddab9d4c19cfdd587ac22850b365b
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-14 23:19:34 +0400

    Update Heap.py

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -10,7 +10,10 @@ class Heap:
         Метод создания массива кучи HeapArray из заданного массива 'a'
         Параметр 'depth' - глубина кучи
         '''
-        pass
+        # depth = self. Get_size_depth(a)[0]
+        self.HeapArray = [None] * self.Get_size_depth(a)[1]
+        for node in a:
+            self.Add(node)
 
     def GetMax(self):
         '''
@@ -26,3 +29,26 @@ class Heap:
         '''
         # добавляем новый элемент key в кучу и перестраиваем её
         return False  # если куча вся заполнена
+    
+        def Get_size_depth(self, array):  # дополнительный метод
+        '''
+        Метод расчета и возврата глубины и размера кучи по размеру массива array
+        Возвращает кортеж с параметрами: 0 - глубина кучи, 1 - размер кучи
+        '''
+        if array:
+            depth_heap = 0
+            size_heap = 0
+            while size_heap < len(array):
+                depth_heap += 1
+                size_heap = 2 ** (depth_heap + 1) - 1
+            return depth_heap, size_heap
+        else:
+            return 0, 0
+
+    def move_up(self):  # дополнительный метод
+        '''Метод перемещения элемента вверх'''
+        pass
+
+    def move_sown(self):  # дополнительный метод
+        '''Метод перемещения элемента вниз'''
+        pass
