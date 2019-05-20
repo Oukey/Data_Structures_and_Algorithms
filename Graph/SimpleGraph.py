4a0f7aa9e11278d250c19337e3f952b465fb43ce
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-20 13:39:58 +0400

    Update SimpleGraph.py
    refactoring

--- Graph/SimpleGraph.py
+++ Graph/SimpleGraph.py
@@ -12,14 +12,17 @@ class SimpleGraph:
     def __init__(self, size):
         self.max_vertex = size
         self.m_adjacency = [[0] * size for _ in range(size)]
-        self.vertex = [None] * size
+        self.vertex = [None] * size  # спаисок вершин
 
-    def AddVertex(self, v):
-        '''Метод добавления новой вершины с значением Value'''
+    def AddVertex(self, value):
+        '''Метод добавления новой вершины с значением Value в свободное место self.vertex'''
         # ваш код добавления новой вершины
         # с значением value
         # в свободное место массива vertex
-        pass
+        for ind in range(self.max_vertex):
+            if self.vertex[ind] is None:
+                self.vertex[ind] = value
+                break
 
         # здесь и далее, параметры v -- индекс вершины
         # в списке  vertex
@@ -27,6 +30,11 @@ class SimpleGraph:
     def RemoveVertex(self, v):
         '''Метод удаления вершины со всеми её ребрами'''
         # ваш код удаления вершины со всеми её рёбрами
+        if self.is_vertex(v):
+            for i in range(self.max_vertex):  # Удаление ребер
+                self.RemoveEdge(i, v)
+            self.vertex[v] = None  # удаление вершины
+
         pass
 
     def IsEdge(self, v1, v2):
@@ -34,15 +42,29 @@ class SimpleGraph:
         Метод проверки наличия ребра между вершинами v1 и v2
         return True если ребро есть, иначе return False
         '''
-        # True если есть ребро между вершинами v1 и v2
-        return False
+        # True если есть ребро между вершинами v1 и v2:
+        if self.is_vertex(v1) and self.is_vertex(v2):
+            return self.m_adjacency[v1][v2] == 1
+        else:
+            return False
 
     def AddEdge(self, v1, v2):
         '''Метод добавления ребра между вершинами v1 и v2'''
         # добавление ребра между вершинами v1 и v2
-        pass
+        if self.is_vertex(v1) and self.is_vertex(v2):
+            self.m_adjacency[v1][v2] = 1
+            self.m_adjacency[v2][v1] = 1
+            return True
+        else:
+            return False
 
     def RemoveEdge(self, v1, v2):
         '''Метод удаления ребра между вершинами v1 и v2'''
         # удаление ребра между вершинами v1 и v2
-        pass
+        if self.is_vertex(v1) and self.is_vertex(v2):
+            self.m_adjacency[v1][v2] = 0
+            self.m_adjacency[v2][v1] = 0
+
+    def is_vertex(self, v):
+        '''Метод проверки вершины в списке vertex'''
+        return v <= self.max_vertex - 1 and self.vertex[v]
