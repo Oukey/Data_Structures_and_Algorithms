40ee34fa53b31f9675ab821384081982f69bcecd
Eugene <42565803+Oukey@users.noreply.github.com>
2019-07-01 20:03:13 +0400

    Update SimpleGraph_BFS.py

--- Graph/SimpleGraph_BFS.py
+++ Graph/SimpleGraph_BFS.py
@@ -1,50 +1,64 @@
-# модель построения Графов + метод обхода в глубину + метод обхода в ширину
+# модель построения Графов + метод обхода в глубину + обход вширину
 
-class Stack:  # стек для поиска в глубину
+
+class Stack:
+    '''класс стека для метода обхода в глубину'''
 
     def __init__(self):
         self.stack = []
 
     def pop(self):
-        if self.size() > 0:
-            return self.stack.pop(0)
-        return None
+        '''метод вставки в конец стека'''
+        if len(self.stack) != 0:
+            return self.stack.pop()
+        else:
+            return None
 
     def push(self, value):
-        self.stack.insert(0, value)
+        self.stack.append(value)
 
     def peek(self):
-        if self.size() > 0:
-            return self.stack[0]
-        return None
+        if len(self.stack) != 0:
+            return self.stack[-1]
+        else:
+            return None
 
     def size(self):
+        '''метод возврата размера стека'''
         return len(self.stack)
 
 
-class Queue:  # очередь для поиска в ширину
+class Queue:
+    '''класс очереди для метода обхода в ширину'''
 
     def __init__(self):
-        self.items = []
+        self.list = []
 
     def enqueue(self, item):
-        return self.items.insert(0, item)
+        '''метод вставки в конец очереди'''
+        self.list.append(item)
 
     def dequeue(self):
-        if self.size() > 0:
-            return self.items.pop()
-        return None
+        '''метод изъятия из начала очереди'''
+        # выдача из головы
+        if len(self.list) > 0:
+            return self.list.pop(0)
+        else:
+            return None  # если очередь пустая
 
     def size(self):
-        return len(self.items)
+        '''метод возврата размера очереди'''
+        if len(self.list) == 0:
+            return 0
+        else:
+            return len(self.list)
 
 
 class Vertex:
 
     def __init__(self, val):
         self.Value = val
-        self.Hit = False  # флаг посещения
-        self.Parent = None
+        self.Hit = False
 
     def __repr__(self):
         '''Метод упрощенного отображения объектов класса Vertex'''
@@ -56,37 +70,35 @@ class SimpleGraph:
     def __init__(self, size):
         self.max_vertex = size
         self.m_adjacency = [[0] * size for _ in range(size)]
-        self.vertex = [None] * size  # спаисок вершин
+        self.vertex = []
+
+    def is_vertex(self, v):
+        return v <= self.max_vertex - 1 and self.vertex[v]
 
     def AddVertex(self, value):
         '''Метод добавления новой вершины с значением Value в свободное место self.vertex'''
-        # ваш код добавления новой вершины
-        # с значением value
-        # в свободное место массива vertex
-        for ind in range(self.max_vertex):
-            if self.vertex[ind] is None:
-                self.vertex[ind] = Vertex(value)
-                break
+        self.vertex.append(Vertex(value))
+        if len(self.vertex) > self.max_vertex:
+            [i.append(0) for i in self.m_adjacency]
+            self.max_vertex += 1
+            self.m_adjacency.append([0] * self.max_vertex)
 
-        # здесь и далее, параметры v -- индекс вершины
-        # в списке  vertex
-
-    def RemoveVertex(self, v):
+    def RemoveVertex(self, value):
         '''Метод удаления вершины со всеми её ребрами'''
-        # ваш код удаления вершины со всеми её рёбрами
-        if self.is_vertex(v):
-            for i in range(self.max_vertex):  # Удаление ребер
-                self.RemoveEdge(i, v)
-            self.vertex[v] = None  # удаление вершины
-
-        pass
+        if self.is_vertex(value):
+            [i.pop(value) for i in self.m_adjacency]
+            self.m_adjacency.pop(value)
+            self.vertex.pop(value)
+            self.max_vertex -= 1
+            return True
+        else:
+            return False
 
     def IsEdge(self, v1, v2):
         '''
         Метод проверки наличия ребра между вершинами v1 и v2
         return True если ребро есть, иначе return False
         '''
-        # True если есть ребро между вершинами v1 и v2:
         if self.is_vertex(v1) and self.is_vertex(v2):
             return self.m_adjacency[v1][v2] == 1
         else:
@@ -94,7 +106,6 @@ class SimpleGraph:
 
     def AddEdge(self, v1, v2):
         '''Метод добавления ребра между вершинами v1 и v2'''
-        # добавление ребра между вершинами v1 и v2
         if self.is_vertex(v1) and self.is_vertex(v2):
             self.m_adjacency[v1][v2] = 1
             self.m_adjacency[v2][v1] = 1
@@ -104,67 +115,65 @@ class SimpleGraph:
 
     def RemoveEdge(self, v1, v2):
         '''Метод удаления ребра между вершинами v1 и v2'''
-        # удаление ребра между вершинами v1 и v2
         if self.is_vertex(v1) and self.is_vertex(v2):
             self.m_adjacency[v1][v2] = 0
             self.m_adjacency[v2][v1] = 0
-
-    def is_vertex(self, v):
-        '''Метод проверки вершины в списке vertex'''
-        return v <= self.max_vertex - 1 and self.vertex[v]
+            return True
+        return False
 
     def available_vertices(self, v):
-        '''
-        Метод поиска соседних вершин
-        return: список доступных вершин или False
-        '''
-        for i, val in enumerate(self.m_adjacency[v]):
-            if val == 1 and not self.vertex[i].Hit:
+        for i, value in enumerate(self.m_adjacency[v]):
+            if value == 1 and not self.vertex[i].Hit:
                 return self.vertex[i]
         return False
 
     def DepthFirstSearch(self, VFrom, VTo):
         '''
         Метод поиска кратчайшего пути от VFrom до  VTo
-        return: список вершин пути из VFrom в VTo или [] если пути нет
+        return: список узлов  путь из VFrom в VTo или [] если пути нет
         '''
-        # узлы задаются позициями в списке vertex
-        # возвращается список узлов -- путь из VFrom в VTo
-        # или [] если пути нету
-        # Проверка присутствия обоих узлов в куче:
-        # if VFrom > len(self.vertex):
         stack = Stack()
-        for vert in self.vertex:
-            vert.Hit = False
-        selected_vertex = self.vertex[VFrom]
-        selected_vertex.Hit = True
-        stack.push(selected_vertex)
+        for v in self.vertex:
+            v.Hit = False
+
+        current = self.vertex[VFrom]
+        current.Hit = True
+        stack.push(current)
         while True:
-            VFrom = self.vertex.index(selected_vertex)
+
+            VFrom = self.vertex.index(current)
             if self.m_adjacency[VFrom][VTo] == 1:
                 stack.push(self.vertex[VTo])
                 break
             else:
-                selected_vertex = self.available_vertices(VFrom)
-            if not selected_vertex:
+                current = self.available_vertices(VFrom)
+
+            if not current:
                 stack.pop()
                 if stack.size() == 0:
                     return stack.stack
                 else:
-                    selected_vertex = stack.stack[0]
-                    selected_vertex.Hit = True
+                    current = stack.stack[0]
+                    current.Hit = True
             else:
-                selected_vertex.Hit = True
-                stack.push(selected_vertex)
-        return stack.stack[::-1]
+                current.Hit = True
+                stack.push(current)
+
+        return stack.stack[::]
 
     def get_path_bfs(self, vertex):
+        '''Метод обхода'''
         yield vertex
         if vertex.Parent is None:
             return vertex
         yield from self.get_path_bfs(vertex.Parent)
 
     def BreadthFirstSearch(self, VFrom, VTo):
+        '''
+        Метод получает в качестве параметров индексы в массиве vertex двух узлов
+        и возвращает список узлов, представляющий собой путь из начального узла в конечный,
+        либо пустой список, если пути не существует.
+        '''
         queue = Queue()
         for v in self.vertex:
             v.Hit = False
@@ -185,3 +194,4 @@ class SimpleGraph:
                 adjacent_vertex.Hit = True
                 queue.enqueue(adjacent_vertex)
         return [i for i in self.get_path_bfs(self.vertex[VTo])][::-1]
+    
